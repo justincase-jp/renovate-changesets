20acfd8460ad334c56a332fcc19a76975bbf5716
ハン / Han <16288797+hannoeru@users.noreply.github.com>
2022-10-19 15:49:42 +0900

    fix: skip ci for github action required check

--- src/index.ts
+++ src/index.ts
@@ -225,7 +225,7 @@ ${changeset.summary}
 
   if (!(await checkIfClean())) {
     await commitAll(
-      `chore(deps): updated changesets for modified dependencies`,
+      `chore(deps): updated changesets for modified dependencies [skip ci]`,
     );
     await gitPush();
   }
